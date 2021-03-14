96bf6b85859bb2b43d524c09e45200dcba4005d7
NetradeepakC <netradeepakc@gmail.com>
2021-03-14 16:33:30 +0530

    Movement Machanics added to Map Display

--- Map_display.py
+++ Map_display.py
@@ -19,6 +19,10 @@ for i in range(len(Map.Map)):
 		if(Map.Map[i][j]==2):
 			playerY=length_of_tile*i+length_of_tile/2
 			playerX=length_of_tile*j+length_of_tile/2
+			break
+	if(not playerX==0 or not playerY):
+		break
+print(playerX,playerY)
 
 screen=pygame.display.set_mode((size_of_map*2*length_of_tile,size_of_map*length_of_tile))
 
@@ -26,10 +30,12 @@ screen=pygame.display.set_mode((size_of_map*2*length_of_tile,size_of_map*length_
 ((Image.open("map_asphalt.jpg")).resize((length_of_tile,length_of_tile))).save("map_asphalt_adjusted.jpg")
 ((Image.open("carousel.png")).resize((length_of_tile,length_of_tile))).save("carousel_adjusted.png")
 ((Image.open("map_mud.jpg")).resize((length_of_border,length_of_border))).save("map_mud_adjusted.jpg")
+((Image.open("player.png")).resize((length_of_tile,length_of_tile))).save("player_adjusted.png")
 Grass=pygame.image.load("map_Grass_adjusted.png")
 Asphalt=pygame.image.load("map_asphalt_adjusted.jpg")
 POI=pygame.image.load("carousel_adjusted.png")
 Border=pygame.image.load("map_mud_adjusted.jpg")
+Player_icon=pygame.image.load("player_adjusted.png")
 
 running=True
 
@@ -37,10 +43,19 @@ clock=pygame.time.Clock()
 
 while running:
 	clock.tick(60)
-	
+		
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
 			running=False
+	pressed_keys=pygame.key.get_pressed()
+	if pressed_keys[pygame.K_UP] and (not Map.Map[int((playerY-length_of_tile/16)//length_of_tile)][int(playerX//length_of_tile)]==0):
+		playerY-=length_of_tile/16;
+	if pressed_keys[pygame.K_DOWN] and (not Map.Map[int((playerY+length_of_tile/16)//length_of_tile)][int(playerX//length_of_tile)]==0):
+		playerY+=length_of_tile/16;
+	if pressed_keys[pygame.K_LEFT] and (not Map.Map[int(playerY//length_of_tile)][int((playerX-length_of_tile/16)//length_of_tile)]==0):
+		playerX-=length_of_tile/16;
+	if pressed_keys[pygame.K_RIGHT] and (not Map.Map[int(playerY//length_of_tile)][int((playerX+length_of_tile/16)//length_of_tile)]==0):
+		playerX+=length_of_tile/16;
 	
 	disp_x=0
 	disp_y=0
@@ -52,36 +67,27 @@ while running:
 				screen.blit(Grass,(disp_x,disp_y))
 				
 				if(i>0):
-					
 					if(j>0 and Map.Map[i-1][j-1]!=0):
 						screen.blit(Border,(disp_x,disp_y))
-					
 					if(Map.Map[i-1][j]!=0):
 						for k in range(length_of_tile//length_of_border):
 							screen.blit(Border,(disp_x + k*length_of_border,disp_y))
-					
 					if(j<len(Map.Map[0])-1 and Map.Map[i-1][j+1]!=0):
 						screen.blit(Border,(disp_x + length_of_tile - length_of_border,disp_y))
 					
 				if(j>0 and Map.Map[i][j-1]!=0):
-					
 					for k in range(length_of_tile//length_of_border):
 						screen.blit(Border,(disp_x,disp_y + k*length_of_border))
-				
 				if(j<len(Map.Map[0])-1 and Map.Map[i][j+1]!=0):
-					
 					for k in range(length_of_tile//length_of_border):
 						screen.blit(Border,(disp_x + length_of_tile - length_of_border,disp_y + k*length_of_border))
 				
 				if(i<len(Map.Map)-1):
-					
 					if(j>0 and Map.Map[i+1][j-1]!=0):
 						screen.blit(Border,(disp_x,disp_y+ length_of_tile - length_of_border))
-					
 					if(Map.Map[i+1][j]!=0):
 						for k in range(length_of_tile//length_of_border):
 							screen.blit(Border,(disp_x + k*length_of_border,disp_y+ length_of_tile - length_of_border))
-					
 					if(j<len(Map.Map[0])-1 and Map.Map[i+1][j+1]!=0):
 						screen.blit(Border,(disp_x + length_of_tile - length_of_border,disp_y+ length_of_tile - length_of_border))
 				
@@ -92,5 +98,7 @@ while running:
 			disp_x+=length_of_tile
 		disp_x=0
 		disp_y+=length_of_tile
+		
+		screen.blit(Player_icon,(playerX-length_of_tile/2,playerY-length_of_tile/2))
 	
 	pygame.display.update()
